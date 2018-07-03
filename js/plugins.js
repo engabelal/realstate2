9bb4f0c7985d0d3b4705a8b2fb8bcd0ff8e3b995
Ahmed Belal <eng.abelal@gmail.com>
2018-07-03 14:06:45 +0200

    realstate2

---
+++ js/plugins.js
@@ -0,0 +1,14 @@
+//resize carousal screen view
+
+$(function () {
+
+//         'use strict';
+//         var winh = $(window).height(),
+//             upperh = $('.before-nav').innerHeight(),
+//             searchh =$('.search').innerHeight(),
+//             navh = $('.main-nav').innerHeight();
+// //        if (screen.width >= 600) {
+//             $('.carousel ,.carousel-item').height(winh - (upperh + navh+ searchh));
+// //        }
+    })
+    //------------------
